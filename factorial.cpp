564ce63eb7e437808cba56e999f1d52964e2662f
[KA HOU CHAN] <[kahou76@uw.edu]>
2021-10-03 23:09:46 -0700

    changed again

--- factorial.cpp
+++ factorial.cpp
@@ -13,18 +13,17 @@ using namespace std;
 int fact(int n) {
   int foo = 200;
   if (n < 30) {
-    if (n <= 1){
-      return 1;}
+    if (n <= 1) {
+      return 1;
+    }
     return n * fact(n - 1);
-  }  
-    cout << "Too large: " << n << endl;
-    return -1;
-  
+  }
+  cout << "Too large: " << n << endl;
+  return -1;
 }
 
 int memoryLeakFunction() {
-  
-  
+
   cout << "Hello World" << endl;
   cout << "Fact 5: " << fact(5) << endl;
   // memory leak if we do not delete it
